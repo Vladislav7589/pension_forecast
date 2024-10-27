a60d700549669c33e6a048ace440c3305de9c842
imarachedder <marufmart98@gmail.com>
2024-10-27 07:30:34 +0600

    ввв

--- pension_forecast/urls.py
+++ pension_forecast/urls.py
@@ -38,7 +38,9 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("info", views.welcome_view, name="info"),
     path('admin/', admin.site.urls),
-    path('predict/', DatasetPredictionView.as_view(), name='predict'),
+    # path('predict/', DatasetPredictionView.as_view(), name='predict'),
+    path('predict/', DatasetPredictionView.as_view({'post': 'post'}), name='predict'),
+    path('predict/results/', DatasetPredictionView.as_view({'get': 'get'}), name='predict-results'),  # GET для получения результатов
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
 ]
